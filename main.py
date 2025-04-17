1c4ed5577fe20e90e06c61f2dab0081850b770a6
v <wang78906@yahoo.com.tw>
2025-04-17 19:07:47 +0800

    fix

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ HEADERS = {
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/120.0.0.0 Safari/537.36"
+        "Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0"
     )
 }
 TG_TOKEN = os.environ.get("TG_TOKEN")
