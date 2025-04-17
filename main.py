8e4abc3c9ab023bdb738ce207f266f45e1b18166
v <wang78906@yahoo.com.tw>
2025-04-17 16:03:39 +0800

    fix soup bug

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ TG_TOKEN = os.environ.get("TG_TOKEN")
 TG_CHAT_ID = os.environ.get("TG_CHAT_ID")
 STATE_FILE = "last_sent.txt"
 
+
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage"
     data = {
@@ -21,28 +22,33 @@ def send_telegram_message(message):
     else:
         print("❌ 傳送失敗：", response.text)
 
+
 def load_last_url():
     if not os.path.exists(STATE_FILE):
         return None
     with open(STATE_FILE, "r", encoding="utf-8") as f:
         return f.read().strip()
 
+
 def save_last_url(url):
     with open(STATE_FILE, "w", encoding="utf-8") as f:
         f.write(url)
 
+
 def commit_last_url():
     subprocess.run(["git", "config", "--global", "user.name", "ptt-bot"])
-    subprocess.run(["git", "config", "--global", "user.email", "ptt-bot@example.com"])
+    subprocess.run(["git", "config", "--global",
+                   "user.email", "ptt-bot@example.com"])
     subprocess.run(["git", "add", STATE_FILE])
     subprocess.run(["git", "commit", "-m", "update last_sent url"])
     subprocess.run(["git", "push"])
+
 
 def check_new_posts():
     last_url = load_last_url()
     res = requests.get(PTT_URL, headers=HEADERS)
     soup = BeautifulSoup(res.text, "html.parser")
-    print("soup=" + soup)
+    print("soup=" + str(soup))
     articles = soup.select("div.title a")
     print("articles=" + articles)
 
@@ -78,6 +84,7 @@ def check_new_posts():
     latest_url = new_articles[0][1]
     save_last_url(latest_url)
     commit_last_url()
+
 
 if __name__ == "__main__":
     check_new_posts()
